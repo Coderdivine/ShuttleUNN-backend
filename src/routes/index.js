689abbb6d7dc81d3d4b90f3d910fd05be952b36c
Mac <chimdi4332@gmail.com>
2023-08-12 11:32:43 +0100

    USER controller and router enabled

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
-router.use(require("./../middlewares/trim-incoming-requests.middleware"))
+//router.use(require("./../middlewares/trim-incoming-requests.middleware"))
 router.use("/user", require("./user.route"));
-router.use("/flw", require("./flutterwave.route"));
 
 router.get("/", (req, res) => {
-    return res.status(200).json({ message: "Hello world from [https://redesign.axgura.com]" });
+    return res.status(200).json({ message: "Hello world from [https://devsensor.com]" });
 });
 
 module.exports = router;
