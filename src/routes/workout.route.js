377eb2711bc588234caed74c324230cbd9b8fb41
Mac <chimdi4332@gmail.com>
2023-08-12 19:04:36 +0100

    Enabled Service, Controller and Route for Workout

--- src/routes/workout.route.js
+++ src/routes/workout.route.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth.middleware")();
-const UserCtrl = require("../controllers/user.controller");
 const sharp = require("sharp");
+const WorkoutCtrl = require("../controllers/workout.controller");
+
+
+router.post("/workout", WorkoutCtrl.addWorkout);
+router.get("/workouts", WorkoutCtrl.getWorkouts);
+router.get("/workout", WorkoutCtrl.getWorkout);
+router.post("/delete", WorkoutCtrl.deleteWorkout);
+router.post("/update", WorkoutCtrl.updateWorkout);
+router.post("/save", Workout.saveWorkout);
+router.post("/add-new-workout-to-workouts", WorkoutCtrl.addNewWorkoutInWorkouts);
+router.post("/edit-workout-in-workouts", WorkoutCtrl.editWorkoutInWorkouts);
+router.post("/delete-workout-in-workouts", WorkoutCtrl.deleteFromWorkoutInWorkouts);
 
 
 
