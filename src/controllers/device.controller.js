1af949a978d2b55b01ee85b7d19cd23d9919a4e3
Mac <chimdi4332@gmail.com>
2023-08-16 16:23:06 +0100

    enabled Service and Controller for device

--- src/controllers/device.controller.js
+++ src/controllers/device.controller.js
@@ -4,149 +4,166 @@ const CustomError = require("../utils/custom-error");
 
 class UserContoller {
 
-    async getWifiStrength(req, res) {
-        try {
-            if(!req.params) throw new CustomError("No parameter passed.", 400);
-            const result = await DeviceService.getWifiStrength({ devsensor_id: req.params.devsensor_id, wifi_strength: req.params.wifi_strength })
-            res.status(200).send(response("Wifi strength detected",result));
-        } catch (error) {
-            throw new CustomError(
-                "An issue has arisen. Please try again later.",
-                500
-              );
-        }
+  async getWifiStrength(req, res) {
+    try {
+      if (!req.params) throw new CustomError("No parameter passed.", 400);
+      const result = await DeviceService.getWifiStrength({
+        devsensor_id: req.params.devsensor_id,
+        wifi_strength: req.params.wifi_strength,
+      });
+      res.status(200).send(response("Wifi strength detected", result));
+    } catch (error) {
+      throw new CustomError(
+        "An issue has arisen. Please try again later.",
+        500
+      );
     }
+  }
 
-    async getBatteryLevel(req, res) {
-        try {
-            if(!req.params) throw new CustomError("No parameter passed.", 400);
-            const { devsensor_id, battery_level } = req.params;
-            const result = await DeviceService.getBatteryLevel({ devsensor_id, battery_level });
-            res.status(201).send(response("Battery Level",result));
-        } catch (error) {
-            throw new CustomError(
-                "An issue has arisen. Please try again later.",
-                500
-              );
-        }
+  async getBatteryLevel(req, res) {
+    try {
+      if (!req.params) throw new CustomError("No parameter passed.", 400);
+      const { devsensor_id, battery_level } = req.params;
+      const result = await DeviceService.getBatteryLevel({
+        devsensor_id,
+        battery_level,
+      });
+      res.status(201).send(response("Battery Level", result));
+    } catch (error) {
+      throw new CustomError(
+        "An issue has arisen. Please try again later.",
+        500
+      );
     }
+  }
 
-    async getBatteryHealth(req, res) {
-        try {
-            if(!req.params) throw new CustomError("No parameter passed.", 400);
-            const { devsensor_id, battery_health } = req.params;
-            const result = await DeviceService.getBatteryHealth({ devsensor_id, battery_health });
-            res.status(201).send(response("Battery health.",result));
-        } catch (error) {
-            throw new CustomError(
-                "An issue has arisen. Please try again later.",
-                500
-              );
-        }
+  async getBatteryHealth(req, res) {
+    try {
+      if (!req.params) throw new CustomError("No parameter passed.", 400);
+      const { devsensor_id, battery_health } = req.params;
+      const result = await DeviceService.getBatteryHealth({
+        devsensor_id,
+        battery_health,
+      });
+      res.status(201).send(response("Battery health.", result));
+    } catch (error) {
+      throw new CustomError(
+        "An issue has arisen. Please try again later.",
+        500
+      );
     }
+  }
 
-    async getDevicehealth(req, res) {
-        try {
-            if(!req.params) throw new CustomError("No parameter passed.", 400);
-            const { devsensor_id, device_health } = req.params;
-            const result = await DeviceService.getDevicehealth({ devsensor_id, device_health });
-            res.status(201).send(response("Device health.",result));
-        } catch (error) {
-            throw new CustomError(
-                "An issue has arisen. Please try again later.",
-                500
-              );
-        }
+  async getDevicehealth(req, res) {
+    try {
+      if (!req.params) throw new CustomError("No parameter passed.", 400);
+      const { devsensor_id, device_health } = req.params;
+      const result = await DeviceService.getDevicehealth({
+        devsensor_id,
+        device_health,
+      });
+      res.status(201).send(response("Device health.", result));
+    } catch (error) {
+      throw new CustomError(
+        "An issue has arisen. Please try again later.",
+        500
+      );
     }
+  }
 
-    async getDeviceMemory(req, res) {
-        try {
-            if(!req.params) throw new CustomError("No parameter passed.", 400);
-            const { devsensor_id, device_memory } = req.params;
-            const result = await DeviceService.getDeviceMemory({ devsensor_id, device_memory });
-            res.status(201).send(response("Device memory.",result));
-        } catch (error) {
-            throw new CustomError(
-                "An issue has arisen. Please try again later.",
-                500
-              );
-        }
+  async getDeviceMemory(req, res) {
+    try {
+      if (!req.params) throw new CustomError("No parameter passed.", 400);
+      const { devsensor_id, device_memory } = req.params;
+      const result = await DeviceService.getDeviceMemory({
+        devsensor_id,
+        device_memory,
+      });
+      res.status(201).send(response("Device memory.", result));
+    } catch (error) {
+      throw new CustomError(
+        "An issue has arisen. Please try again later.",
+        500
+      );
     }
+  }
 
-    async getDeviceAge(req, res) {
-        try {
-            if(!req.params) throw new CustomError("No parameter passed.", 400);
-            const { devsensor_id, device_age } = req.params;
-            const result = await DeviceService.getDeviceAge({ devsensor_id, device_age });
-            res.status(201).send(response("Device Age.",result));
-        } catch (error) {
-            throw new CustomError(
-                "An issue has arisen. Please try again later.",
-                500
-              );
-        }
+  async getDeviceAge(req, res) {
+    try {
+      if (!req.params) throw new CustomError("No parameter passed.", 400);
+      const { devsensor_id, device_age } = req.params;
+      const result = await DeviceService.getDeviceAge({
+        devsensor_id,
+        device_age,
+      });
+      res.status(201).send(response("Device Age.", result));
+    } catch (error) {
+      throw new CustomError(
+        "An issue has arisen. Please try again later.",
+        500
+      );
     }
+  }
 
-    async trackFrequency(req, res) {
-        try {
-            if(!req.params) throw new CustomError("No parameter passed.", 400);
-            const { devsensor_id, frequency } = req.params;
-            const result = await DeviceService.trackFrequency({ devsensor_id, frequency });
-            res.status(201).send(response("Track frequency.",result));
-        } catch (error) {
-            throw new CustomError(
-                "An issue has arisen. Please try again later.",
-                500
-              );
-        }
+  async trackFrequency(req, res) {
+    try {
+      if (!req.params) throw new CustomError("No parameter passed.", 400);
+      const { devsensor_id, frequency } = req.params;
+      const result = await DeviceService.trackFrequency({
+        devsensor_id,
+        frequency,
+      });
+      res.status(201).send(response("Track frequency.", result));
+    } catch (error) {
+      throw new CustomError(
+        "An issue has arisen. Please try again later.",
+        500
+      );
     }
+  }
 
-
-    async disableDevice(req, res) {
-        try {
-            if(!req.params) throw new CustomError("No parameter passed.", 400);
-            const { devsensor_id } = req.params;
-            const result = await DeviceService.disableDevice({ devsensor_id });
-            res.status(201).send(response("Dev Sensor disabled.",result));
-        } catch (error) {
-            throw new CustomError(
-                "An issue has arisen. Please try again later.",
-                500
-              );
-        }
+  async disableDevice(req, res) {
+    try {
+      if (!req.params) throw new CustomError("No parameter passed.", 400);
+      const { devsensor_id } = req.params;
+      const result = await DeviceService.disableDevice({ devsensor_id });
+      res.status(201).send(response("Dev Sensor disabled.", result));
+    } catch (error) {
+      throw new CustomError(
+        "An issue has arisen. Please try again later.",
+        500
+      );
     }
+  }
 
-    async enableDevice(req, res) {
-        try {
-            if(!req.params) throw new CustomError("No parameter passed.", 400);
-            const { devsensor_id } = req.params;
-            const result = await DeviceService.enableDevice({ devsensor_id });
-            res.status(201).send(response("Developer Sensor enabled.",result));
-        } catch (error) {
-            throw new CustomError(
-                "An issue has arisen. Please try again later.",
-                500
-              );
-        }
+  async enableDevice(req, res) {
+    try {
+      if (!req.params) throw new CustomError("No parameter passed.", 400);
+      const { devsensor_id } = req.params;
+      const result = await DeviceService.enableDevice({ devsensor_id });
+      res.status(201).send(response("Developer Sensor enabled.", result));
+    } catch (error) {
+      throw new CustomError(
+        "An issue has arisen. Please try again later.",
+        500
+      );
     }
+  }
 
-
-    async disconnectWifi(req, res) {
-        try {
-            if(!req.params) throw new CustomError("No parameter passed.", 400);
-            const { devsensor_id } = req.params;
-            const result = await DeviceService.disconnectWifi({ devsensor_id });
-            res.status(201).send(response("Wifi Disconnected.",result));
-        } catch (error) {
-            throw new CustomError(
-                "An issue has arisen. Please try again later.",
-                500
-              );
-        }
+  async disconnectWifi(req, res) {
+    try {
+      if (!req.params) throw new CustomError("No parameter passed.", 400);
+      const { devsensor_id } = req.params;
+      const result = await DeviceService.disconnectWifi({ devsensor_id });
+      res.status(201).send(response("Wifi Disconnected.", result));
+    } catch (error) {
+      throw new CustomError(
+        "An issue has arisen. Please try again later.",
+        500
+      );
     }
-
-
-    
+  }
+  
 }
 
 module.exports = new UserContoller();
--- src/routes/device.route.js
+++ src/routes/device.route.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth.middleware")();
-const PostureCtrl = require("../controllers/posture.controller");
+const DeviceCtrl = require("../controllers/device.controller");
 
-router.post("/add", PostureCtrl.addPostures);
+router.get("/wifi-strength", DeviceCtrl.getWifiStrength);
+router.get("/battery-level", DeviceCtrl.getBatteryLevel);
+router.get("/battery-health", DeviceCtrl.getBatteryHealth);
+router.get("/device-health", DeviceCtrl.getDevicehealth);
+router.get("/device-memory", DeviceCtrl.getDeviceMemory);
+router.get("/device-age", DeviceCtrl.getDeviceAge);
+router.get("/track-frequency", DeviceCtrl.trackFrequency);
+router.get("/disable-device", DeviceCtrl.disableDevice);
+router.get("/enable-device", DeviceCtrl.enableDevice);
+router.get("/disconnect-wifi", DeviceCtrl.disconnectWifi);
+
 
 module.exports = router;
