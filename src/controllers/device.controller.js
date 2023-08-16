eada99bdee9168b4617549171359b8fb1b132ee6
Mac <chimdi4332@gmail.com>
2023-08-16 10:48:13 +0100

    Enabled Controller for device

--- src/controllers/device.controller.js
+++ src/controllers/device.controller.js
@@ -1,27 +1,35 @@
 const response = require("../utils/response");
-const UserService = require("../services/user.service");
+const DeviceService = require("../services/device.service");
 const CustomError = require("../utils/custom-error");
 
 class UserContoller {
 
-    async register(req, res){
-        try{
-            if(!req.body) throw new CustomError("No request body",404);
-            if(!req.body.email) throw new CustomError("Email is required",404);
-            if(!req.body.password) throw new CustomError("Password is required",404);
-            console.log({ body:req.body })
-            const result = await UserService.register(req.body);
-            if(!result) throw new CustomError("Oops! Registration failed",500);
-            res.status(201).send(response("Registration completed",result));
-        }catch(error){
+    async getWifiStrength(req, res) {
+        try {
+            if(!req.params) throw new CustomError("No parameter passed.", 400);
+            const result = await DeviceService.getWifiStrength({ devsensor_id: req.params.devsensor_id, wifi_strength: req.params.wifi_strength })
+            res.status(200).send(response("Wifi strength detected",result));
+        } catch (error) {
             throw new CustomError(
                 "An issue has arisen. Please try again later.",
                 500
               );
         }
     }
-    
-    async smth() {}
+
+    async getBatteryLevel(req, res) {
+        try {
+            if(!req.params) throw new CustomError("No parameter passed.", 400);
+            const result = await DeviceService.getBatteryLevel(req.params);
+            res.status(201).send(response("Registration completed",result));
+        } catch (error) {
+            throw new CustomError(
+                "An issue has arisen. Please try again later.",
+                500
+              );
+        }
+    }
+
 
     
 }
